99cb2651c1b9e270d72d78e9c4a6b9ab90569c9f
aayushi singh <aayushisingh7786@gmail.com>
2026-01-20 09:08:52 +0530

    add some changes

---
+++ core/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# View for the Home Page
+def home(request):
+    """
+    Renders the 'home.html' template for the home page.
+    """
+    # Assuming 'home.html' is in the templates directory of an app
+    return render(request, 'home.html')
+
+# View for the About Page
+def about(request):
+    """
+    Renders the 'about.html' template for the about page.
+    """
+    # Assuming 'about.html' is in the templates directory of an app
+    return render(request, 'about.html')
+
+# View for the Contact Page
+def contact(request):
+    """
+    Renders the 'contact.html' template for the contact page.
+    """
+    # Assuming 'contact.html' is in the templates directory of an app
+    return render(request, 'contact.html')
